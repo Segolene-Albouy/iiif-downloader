063888d22badfde95f3a752e0893d81517925a77
Segolene-Albouy <segolene.albouy@gmail.com>
2024-12-11 16:23:38 +0100

    [PACKAGE] upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="iiif-download",  # Changed to match desired pip install name
-    version="0.1.2",
+    version="0.1.3",
     author="Segolene-Albouy",
     author_email="segolene.albouy@gmail.com",
     description="A Python package to download images from IIIF manifests",
